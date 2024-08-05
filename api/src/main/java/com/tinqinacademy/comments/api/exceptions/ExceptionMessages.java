bd1fb2bb1f935e2adb9468bf1010bcf45eb00c54
Ivan Georgiev <105276674+tyrxuya@users.noreply.github.com>
2024-08-05 11:36:08 +0300

    [CS-002] Implement operations logic in core layer (#6)
    * Changed roomId to roomNo in Comment entity.
    
    * Added converter and WebConfig
    
    * Forgot to return result in converter (bugfix)
    
    * Added new converter CommentToCommentOutput
    
    * Created exception classes
    
    * Implemented operations

---
+++ api/src/main/java/com/tinqinacademy/comments/api/exceptions/ExceptionMessages.java
@@ -0,0 +1,5 @@
+package com.tinqinacademy.comments.api.exceptions;
+
+public class ExceptionMessages {
+    public static final String COMMENT_NOT_FOUND = "Comment not found";
+}
--- api/src/main/java/com/tinqinacademy/comments/api/operations/usereditcomment/UserEditCommentInput.java
+++ api/src/main/java/com/tinqinacademy/comments/api/operations/usereditcomment/UserEditCommentInput.java
@@ -1,5 +1,6 @@
 package com.tinqinacademy.comments.api.operations.usereditcomment;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tinqinacademy.comments.api.base.OperationInput;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,6 +13,9 @@ import lombok.*;
 @Builder
 @ToString
 public class UserEditCommentInput implements OperationInput {
+    @JsonIgnore
+    private String commentId;
+
     @NotBlank(message = "content cannot be blank!")
     @Size(min = 1, max = 200, message = "content cannot be over 200 symbols!")
     private String content;
