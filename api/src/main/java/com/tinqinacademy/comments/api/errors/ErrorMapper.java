ea589826c37d4edf4ef0b634ae53a85298c6464f
Ivan Georgiev <ivan2003444@gmail.com>
2024-08-09 11:52:24 +0300

    Added logs in ErrorMapper

--- api/src/main/java/com/tinqinacademy/comments/api/errors/ErrorMapper.java
+++ api/src/main/java/com/tinqinacademy/comments/api/errors/ErrorMapper.java
@@ -1,6 +1,7 @@
 package com.tinqinacademy.comments.api.errors;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -9,16 +10,21 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@Slf4j
 @Component
 public class ErrorMapper {
     private final ErrorsList errorsList;
 
     public <T extends Throwable> ErrorsList map(T throwable, HttpStatus httpStatus) {
+        log.info("Mapping throwable {} to ErrorsList", throwable.getClass().getName());
+
         errorsList.setErrors(List.of(Error.builder()
                 .message(throwable.getMessage())
                 .build()));
 
         errorsList.setStatus(httpStatus);
+
+        log.info("Result: {}", errorsList);
 
         return errorsList;
     }
